318886641cdffe6ede42adc4f7edc78d3d3fbb30
ChyoRi <chyori_@naver.com>
2025-09-10 11:57:28 +0900

    UpDate

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? './' : '/',
+  base: command === 'build' ? '/pdms-test/' : '/',
   build: {
     assetsInlineLimit: 0, // ⛔ data:image → ✅ URL
   },
