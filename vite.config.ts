f3929dd2938aa73b70c8301e25db4fd6a182b86e
ChyoRi <chyori_@naver.com>
2026-01-13 16:40:18 +0900

    vite.config.ts build 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/pdms/' : '/',
-  build: {
-    assetsInlineLimit: 0, // ⛔ data:image → ✅ URL
-  },
-  plugins: [react()],
-}))
+export default defineConfig(({ command, mode }) => {
+  // build일 때 mode로 base 결정
+  const base =
+    command === "build"
+      ? mode === "development"
+        ? "/pdms-dev/"
+        : "/pdms/"
+      : "/";
+
+  return {
+    base,
+    build: {
+      assetsInlineLimit: 0,
+    },
+    plugins: [react()],
+  };
+});
